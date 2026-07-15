bf5afabf1cb4c08807ba1e473e719596ba4e2bf5
mahbubjisan34 <mahbubjisan34@gmail.com>
2026-07-15 11:47:15 +0600

    updated project

--- Last Lab/script.js
+++ Last Lab/script.js
@@ -1,52 +1,52 @@
-let form = document.getElementById("studentForm");
+var form = document.getElementById("studentForm");
 
 form.onsubmit = function(e){
 
     e.preventDefault();
 
-    let fn = document.getElementById("fname").value;
-    let ln = document.getElementById("lname").value;
-    let id = document.getElementById("sid").value;
-    let mail = document.getElementById("email").value;
-    let credit = document.getElementById("credit").value;
-    let dept = document.getElementById("department").value;
+    var fn = document.getElementById("fname").value;
+    var ln = document.getElementById("lname").value;
+    var id = document.getElementById("sid").value;
+    var mail = document.getElementById("email").value;
+    var credit = document.getElementById("credit").value;
+    var dept = document.getElementById("department").value;
 
-    let msg = document.getElementById("error");
+    var msg = document.getElementById("error");
     msg.innerHTML = "";
 
-    if(fn.trim()==""){
-        msg.innerHTML="First Name is required";
+    if(fn.trim() == ""){
+        msg.innerHTML = "First Name is required";
         return;
     }
 
-    if(ln.trim()==""){
-        msg.innerHTML="Last Name is required";
+    if(ln.trim() == ""){
+        msg.innerHTML = "Last Name is required";
         return;
     }
 
-    if(id.indexOf("-")==-1){
-        msg.innerHTML="Student ID must contain -";
+    if(id.indexOf("-") == -1){
+        msg.innerHTML = "Student ID must contain -";
         return;
     }
 
-    if(mail.indexOf("@student.aiub.edu")==-1){
-        msg.innerHTML="Invalid Email";
+    if(mail.indexOf("@student.aiub.edu") == -1){
+        msg.innerHTML = "Invalid Email";
         return;
     }
 
-    if(credit<0 || credit>=148){
-        msg.innerHTML="Credit must be 0 to 147";
+    if(credit < 0 || credit >= 148){
+        msg.innerHTML = "Credit must be 0 to 147";
         return;
     }
 
-    if(dept==""){
-        msg.innerHTML="Select Department";
+    if(dept == ""){
+        msg.innerHTML = "Select Department";
         return;
     }
 
-    let table = document.getElementById("studentTable");
+    var table = document.getElementById("studentTable");
 
-    let row = table.insertRow();
+    var row = table.insertRow();
 
     row.insertCell(0).innerHTML = fn;
     row.insertCell(1).innerHTML = ln;
